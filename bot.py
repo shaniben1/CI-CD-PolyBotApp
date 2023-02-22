9a2dc6799c135eb6a112d15b9c11559358db67f2
Shani <shaniben5253@gmail.com>
2023-02-22 14:38:43 +0200

    Shani-PolyBot

--- bot.py
+++ bot.py
@@ -41,6 +41,7 @@ class Bot:
 class QuoteBot(Bot):
     def _message_handler(self, update, context):
         to_quote = True
+
         if update.message.text == 'Don\'t quote me please':
             to_quote = False
 
